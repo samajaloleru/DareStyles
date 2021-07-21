f64a766b29634dd05f7db77d2863ca382656717c
samajaloleru <samsonajaloleru@gmail.com>
2021-07-21 13:05:10 +0100

    first commit

--- public/index.php
+++ public/index.php
@@ -58,3 +58,7 @@ $response = $kernel->handle(
 $response->send();
 
 $kernel->terminate($request, $response);
+if((!empty( $_SERVER['HTTP_X_FORWARDED_HOST'])) || (!empty( $_SERVER['HTTP_X_FORWARDED_FOR'])) ) {
+ 	$_SERVER['HTTP_HOST'] = $_SERVER['HTTP_X_FORWARDED_HOST'];
+ 	$_SERVER['HTTPS'] = 'on';
+ }
